df36b5a9406ee20db68867fb370faff0c4be6c49
Nico Koenig <nico.koenig@fullstax.de>
2020-06-09 11:59:09 +0200

    Task: Create an Info-Box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [AppComponent, NavigationComponent, InfoBoxComponent],
   imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/info-box/info-box.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.scss'],
+})
+export class InfoBoxComponent implements OnInit {
+  isHidden = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggle() {
+    this.isHidden = !this.isHidden;
+  }
+}
