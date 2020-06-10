57f0f71d1eafc28ddfb951a8c4721f82bb10b3e7
Nico Koenig <nico.koenig@fullstax.de>
2020-06-10 10:34:39 +0200

    Task: Add a (titleClicked) @Output

--- src/app/title-box/title-box.component.ts
+++ src/app/title-box/title-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-title-box',
@@ -7,8 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TitleBoxComponent implements OnInit {
   @Input() title = 'Ich bin ein Titel 🤷‍♂️';
+  @Output() titleClicked = new EventEmitter<string>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  handleClick() {
+    this.titleClicked.emit('Grüß Gott 🧐');
+  }
 }
