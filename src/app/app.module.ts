7e38374fce38969791ba2a84215239479aa1098d
Nico Koenig <nico.koenig@fullstax.de>
2020-06-10 09:47:10 +0200

    Task: Output mouse cursor position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, InfoBoxComponent],
+  declarations: [AppComponent, NavigationComponent, InfoBoxComponent, MouseCursorComponent],
   imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
