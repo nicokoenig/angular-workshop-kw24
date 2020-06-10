0cc7f080f1e0c7d529a78d5244e2fbafbb4441d2
Nico Koenig <nico.koenig@fullstax.de>
2020-06-10 11:09:15 +0200

    Task: Use *ngFor

---
+++ src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book.interface';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent implements OnInit {
+  books: Book[] = [
+    {
+      title: 'Design Patterns',
+      subtitle: 'Elements of Reusable Object-Oriented Software',
+    },
+    {
+      title: 'REST und HTTP',
+      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web',
+    },
+    {
+      title: 'Eloquent JavaScript',
+      subtitle: 'A Modern Introduction to Programming',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
