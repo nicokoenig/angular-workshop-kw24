2f2a43f47f61aae70e409ceb1b29d4a75a32df0b
Nico Koenig <nico.koenig@fullstax.de>
2020-06-11 11:57:58 +0200

    Task: Use the async pipe

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,43 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Book } from './book.interface';
 import { BookDataService } from '../book-data.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit, OnDestroy {
-  subscription: Subscription = Subscription.EMPTY;
+export class BookListComponent {
+  books$: Observable<Book[]>;
 
-  books: Book[] = [];
-
-  constructor(private bookData: BookDataService) {}
-
-  ngOnInit(): void {
-    const nextFn = (books: Book[]) => {
-      console.log('Neue Bücher 👍: ', books);
-      this.books = books;
-    };
-
-    this.subscription = this.bookData.getBooks().subscribe(
-      nextFn,
-      (err) => {
-        console.error('Gibt heute keine Bücher 😭', err);
-      },
-      () => {
-        console.log('Complete 🔥');
-      }
-    );
-
-    // Alternative Lösung
-    // this.bookData.getSingleBooks().subscribe((book) => {
-    //   this.books.push(book);
-    // });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  constructor(private bookData: BookDataService) {
+    this.books$ = this.bookData.getBooks();
   }
 }
